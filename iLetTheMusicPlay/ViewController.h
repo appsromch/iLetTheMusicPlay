733996f3c3ab21b88b7f63c7f1428dfad7d003f7
ivelinivanov <ivelin.ivanov@mentormate.com>
2013-09-23 14:28:01 +0300

    Initial Commit

---
+++ iLetTheMusicPlay/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iLetTheMusicPlay
+//
+//  Created by Ivelin Ivanov on 9/23/13.
+//  Copyright (c) 2013 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
